395bdf15e085f63c2fdcf1429b3ee6b72acea8a6
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-06-18 14:15:35 +0200

    build: add base project files feat: create objects array

---
+++ js/myscript.js
@@ -0,0 +1,46 @@
+// Creare l’array di oggetti con le informazioni fornite (nome, ruolo e foto)
+const ourTeam = [
+    {
+        "nomeCognome" : "Wayne Barnett",
+        "role" : "Founder & CEO",
+        "picture" : "img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        "nomeCognome" : "Angela Caroll",
+        "role" : "Chief Editor",
+        "picture" : "img/angela-caroll-chief-editor.jpg"
+    },
+    {
+        "nomeCognome" : "Walter Gordon",
+        "role" : "Office Manager",
+        "picture" : "img/walter-gordon-office-manager.jpg"
+    },
+    {
+        "nomeCognome" : "Angela Lopez",
+        "role" : "Social Media Manager",
+        "picture" : "img/angela-lopez-social-media-manager.jpg"
+    },
+    {
+        "nomeCognome" : "Scott Estrada",
+        "role" : "Developer",
+        "picture" : "img/scott-estrada-developer.jpg"
+    },
+    {
+        "nomeCognome" : "Barbara Ramos",
+        "role" : "Graphic Designer",
+        "picture" : "img/barbara-ramos-graphic-designer.jpg"
+    },
+]
+
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+for(let i = 0; i < ourTeam.length; i++) {
+    let numeroIesimo = ourTeam[i];
+
+
+    for(let key in numeroIesimo) {
+        console.log(key + " : " + numeroIesimo[key]);
+    }
+}
+
+
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
